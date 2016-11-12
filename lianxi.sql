edb421507830997de83ae5ea36efdd6e46326d5a
lizheng167 <lizheng167@qq.com>
2016-11-12 11:51:22 +0800

    更新

--- lianxi.sql
+++ lianxi.sql
@@ -9,11 +9,29 @@
  Target Server Version : 50716
  File Encoding         : utf-8
 
- Date: 11/06/2016 14:47:48 PM
+ Date: 11/12/2016 11:49:51 AM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `station`
+-- ----------------------------
+DROP TABLE IF EXISTS `station`;
+CREATE TABLE `station` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `start_station_name` char(20) NOT NULL,
+  `end_station_name` char(20) NOT NULL,
+  `start_time` char(30) NOT NULL,
+  `arrive_time` char(30) NOT NULL,
+  `tz_num` char(10) NOT NULL,
+  `zy_num` char(10) NOT NULL,
+  `ze_num` char(10) NOT NULL,
+  `lishi` char(30) NOT NULL,
+  `station_train_code` char(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Table structure for `user`
@@ -23,13 +41,17 @@ CREATE TABLE `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(32) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `user`
+--  Table structure for `wiki_urls`
 -- ----------------------------
-BEGIN;
-INSERT INTO `user` VALUES ('1', 'lizheng'), ('2', 'zishan');
-COMMIT;
+DROP TABLE IF EXISTS `wiki_urls`;
+CREATE TABLE `wiki_urls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url_name` varchar(200) NOT NULL,
+  `url_href` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=166 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
